17217a39065dd8819067d9435471d519af977324
leticiamariano123 <127504693+leticiamariano123@users.noreply.github.com>
2024-07-02 08:18:35 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const caixaPrincipal = document.querySelector('.caixa-principal');
+const caixaPerguntas = document.querySelector('.caixa-perguntas');
+const caixaAlternativas = document.querySelector('.caixa-alternativa');
+const caixaResultado = document.querySelector('.caixa-resultado');
+const textoResultado = document.querySelector('.caixa-reultado');
+const perguntas ={
+    
+}
